f9ac12f88f53fa2aa2ad71ba94865f709762c683
Andrew <aelk@users.noreply.github.com>
2015-01-03 21:56:08 -0600

    Update romanToInt.cpp

--- romanToInt.cpp
+++ romanToInt.cpp
@@ -6,12 +6,12 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        int result = 0;
         map<char, int> romans = 
         { 
             { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, 
             { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } 
         };
-        int result = 0;
         
         for (int i = 0; i != s.size(); ++i) {
             if (s[i + 1] && romans[s[i]] < romans[s[i + 1]]) {
@@ -21,7 +21,6 @@ public:
                 result += romans[s[i]];
             }
         }
-        
         return result;
     }
 };
