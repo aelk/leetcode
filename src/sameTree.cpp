33d700e6a9a4e028636a7df42b6528f6107f8545
Andrew <aelk@users.noreply.github.com>
2015-01-17 16:47:04 -0600

    Create sameTree.cpp

---
+++ src/sameTree.cpp
@@ -0,0 +1,14 @@
+/*
+ * LeetCode Problem #100
+ * https://oj.leetcode.com/problems/same-tree/
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (!p && !q) return true;
+        else if ((p && !q) || (!p && q)) return false;
+        else if (p->val != q->val) return false;
+        else return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+};
