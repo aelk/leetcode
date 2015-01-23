b53a2ba3c246f5ed4c1d9531644e06d9050738bd
Andrew <aelk@users.noreply.github.com>
2015-01-22 22:27:39 -0600

    Update minDepth.cpp

--- src/minDepth.cpp
+++ src/minDepth.cpp
@@ -1,22 +1,14 @@
 /*
-* LeetCode Problem #111
-* https://oj.leetcode.com/problems/minimum-depth-of-binary-tree/
-*/
+ * LeetCode Problem #111
+ * https://oj.leetcode.com/problems/minimum-depth-of-binary-tree/
+ */
 
 class Solution {
 public:
     int minDepth(TreeNode *root) {
         if (!root) return 0;
-        
-        int l_depth = minDepth(root->left);
-        int r_depth = minDepth(root->right);
-        
-        if (l_depth == 0) {
-            return r_depth + 1;
-        } else if (r_depth == 0) {
-            return l_depth + 1;
-        } else {
-            return min(l_depth, r_depth) + 1;   
-        }
+        if (!root->left) return minDepth(root->right) + 1;
+        if (!root->right) return minDepth(root->left) + 1;
+        return min(minDepth(root->left), minDepth(root->right)) + 1;
     }
 };
