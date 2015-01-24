7df2766d51f2ff07286ea9acc25b78a2d8d63168
Andrew <aelk@users.noreply.github.com>
2015-01-24 08:38:31 -0600

    Update plusOne.py

--- src/plusOne.py
+++ src/plusOne.py
@@ -6,5 +6,4 @@ class Solution:
         num = 0
         for i in range(len(digits)):
         	num += digits[i] * pow(10, (len(digits)-1-i))
-        num += 1
-        return [int(i) for i in str(num)]
+        return [int(i) for i in str(num+1)]
