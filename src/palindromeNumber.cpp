8ffab3d505857a38a12c1179a4c4f6581e0cb61a
Andrew <aelk@users.noreply.github.com>
2015-07-31 19:04:10 -0400

    Update palindromeNumber.cpp

--- src/palindromeNumber.cpp
+++ src/palindromeNumber.cpp
@@ -2,10 +2,9 @@
  * LeetCode Problem #9
  * https://leetcode.com/problems/palindrome-number/
  */
-
 class Solution {
 public:
-    const int getNthDigit(int num, int num_digits, const int n)
+    const int nthDigit(int num, int num_digits, const int n)
     {
         if (n > num_digits) {
             return -1;
@@ -19,23 +18,27 @@ public:
         return num % 10;
     }
     
+    const int numDigits(int num)
+    {
+        int num_digits = -1;
+        while (num != 0) {
+            num /= 10;
+            ++num_digits;
+        }
+        return num_digits;
+    }
+    
     bool isPalindrome(int x)
     {
         if (x < 0) {
             return false;
         }
     
-        int x_copy = x;
-        int num_digits = -1;
-    
-        while (x_copy != 0) {
-            x_copy /= 10;
-            ++num_digits;
-        }
+        const int num_digits = numDigits(x);
     
         int start = 0, end = num_digits;
         while (start < end) {
-            if (getNthDigit(x, num_digits, start) != getNthDigit(x, num_digits, end)) {
+            if (nthDigit(x, num_digits, start) != nthDigit(x, num_digits, end)) {
                 return false;
             }
             ++start;
